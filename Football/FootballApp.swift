c9eddb61c12b86888e94aeeb63593e6b2ad222b6
gallaugher <john.gallaugher@bc.edu>
2025-01-05 15:34:40 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by John Gallaugher on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
